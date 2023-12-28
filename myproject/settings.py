470ba29c4d649fb2781f03438a37cb08556bb677
chrisfhe <61690210+chrisfhe@users.noreply.github.com>
2023-12-28 16:09:30 +0100

    Added changes to static files

--- myproject/settings.py
+++ myproject/settings.py
@@ -114,8 +114,17 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
+STATICFILES_DIRS = [
+        # specify other static directory where static files are stored n your environment.
+        # by default static folders under each app are always looked into and copied.
+]
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / 'static']  # new
 STATIC_URL = 'static/'
+
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
